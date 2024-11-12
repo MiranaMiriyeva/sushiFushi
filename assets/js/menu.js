3c9634cc3ea9dfa6257bf068ccc74582986ad15f
MiranaMiriyeva <arzumiri20@gmail.com>
2024-11-13 00:45:51 +0400

    menu added

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -27,3 +27,79 @@ responsiveLink.forEach((link) => {
     isOpen = false;
   });
 });
+
+// MENU ITEMS
+let menuItemContainer = document.querySelector(".menu_item__container");
+fetch("https://672381a8493fac3cf24b2055.mockapi.io/sushi")
+  .then((res) => res.json())
+  .then((data) =>
+    data.forEach((item) => {
+      handleTextContent(item);
+    })
+  );
+
+// SELECTING CATEGORIES IN MENU
+let categoriesItems = document.querySelectorAll(".categories_item_value");
+categoriesItems.forEach((value) => {
+  value.addEventListener("click", () => {
+    menuItemContainer.innerHTML = ``;
+    fetch("https://672381a8493fac3cf24b2055.mockapi.io/sushi")
+      .then((res) => res.json())
+      .then((data) =>
+        data.forEach((item) => {
+          if (
+            value.textContent.toLocaleLowerCase() ===
+            item.category.toLocaleLowerCase()
+          ) {
+            handleTextContent(item);
+          } else if (value.textContent.toLocaleLowerCase() === "all menu") {
+            menuItemContainer.innerHTML += `
+      `;
+            handleTextContent(item);
+          }
+        })
+      );
+  });
+});
+
+function handleTextContent(item) {
+  menuItemContainer.innerHTML += `
+  <div class="menu__item">
+    <div class="menu__item__top">
+             <img
+               src=${item.img}
+               alt=${item.name}
+             />
+           
+     <p class="menu__item__name">${item.name}</p>
+     <span>${item.category}</span>
+      <span>${item.piece ? ` | ` + item.piece : ""}</span>
+      <span>${item.kcal ? ` | ` + item.kcal + `kcal` : ""}</span>
+      <p class="menu__item__des">${item.ingredients}</p>
+      </div>
+     <div class="menu__item__ending">
+       <div>
+         <div class="menu__item__new_price">$ ${Math.floor(
+           (item.price * (100 - item.salePercent)) / 100
+         )}.00</div>
+         ${
+           item.salePercent
+             ? `<div class="menu__item__price">${
+                 "$" + item.price + `.00`
+               }</div>`
+             : ""
+         }
+ 
+       </div>
+       <div class="menu__item__basket_button">
+         <i class="fa-solid fa-basket-shopping"></i>
+       </div>
+     </div>
+     ${
+       item.salePercent
+         ? `<div class="menu__item__sale_precent">- ${item.salePercent}%</div>`
+         : ""
+     }   
+   </div>
+       `;
+}
